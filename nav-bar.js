22fd1d1c9ff87b3eac274fd92580d755e93b546d
acchas <178907090+acchas@users.noreply.github.com>
2024-10-18 20:24:46 +0200

    la till click event

--- nav-bar.js
+++ nav-bar.js
@@ -5,8 +5,8 @@ let dropDownList = document.getElementById("dropList");
 dropDownBtn.addEventListener("mouseover",showMenu);
 dropDownBtn.addEventListener("mouseleave", hideMenu);
 /*dropDownBtn.addEventListener("click", goHome);*/
-dropDownBtn.addEventListener("touchmove", showMenu);
-dropDownBtn.addEventListener("touchend", hideMenu);
+dropDownBtn.addEventListener("click", showMenu);
+dropDownBtn.addEventListener("click", hideMenu);
 
 function showMenu(){
   dropDownList.style.display="flex";
