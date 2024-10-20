65979e84ba303c8c6ae149d71f1ea5bbda056c55
acchas <178907090+acchas@users.noreply.github.com>
2024-10-20 19:17:16 +0200

    ska kolla om den går att använda touchscreen

--- nav-bar.js
+++ nav-bar.js
@@ -5,17 +5,22 @@ let dropDownList = document.getElementById("dropList");
 dropDownBtn.addEventListener("mouseover",showMenu);
 dropDownBtn.addEventListener("mouseleave", hideMenu);
 /*dropDownBtn.addEventListener("click", goHome);*/
-dropDownBtn.addEventListener("click", showMenu);
-dropDownBtn.addEventListener("click", hideMenu);
+dropDownBtn.addEventListener("touchmove", showMenu);
+dropDownBtn.addEventListener("touchmove", hideMenu);
+
+/*Kanske sättta en timer på att menyn försvinner? */
 
 function showMenu(){
   dropDownList.style.display="flex";
+
 
 }
 function hideMenu(){
   dropDownList.style.display="none";
 
 }
+
+
 function goHome(){
 
   location.href="index.html";
